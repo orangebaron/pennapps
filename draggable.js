d195de4fc7e215212041a525011e1e093d5d0d0d
orangebaron <saalws@ctemc.org>
2017-09-08 23:23:31 -0400

    WE DID IT REDDIT

--- draggable.js
+++ draggable.js
@@ -3,6 +3,47 @@ var lastY = new Map();
 var posX = new Map(); //map of object to position
 var posY = new Map();
 var currentobj; //object being dragged
+
+function totaloffset(elem) { //get total offset of object
+  var pos = {
+    top: elem.offsetTop,
+    left: elem.offsetLeft
+  };
+  if (elem.offsetParent) {
+    var p = totaloffset(elem.offsetParent);
+    pos.top += p.top;
+    pos.left += p.left;
+  }
+  return pos;
+}
+function getNearestElement() {
+  //find the nearest blue object if it's close enough
+  var elems = document.getElementsByClassName("draggable blue");
+  var minDist = 65;
+  var minElement;
+  for (var i = 0;i<elems.length;i++) {
+    var elem = elems.item(i);
+    if (elem != currentobj) {
+      console.log(elem.offsetTop);
+      var dist = Math.sqrt(Math.pow(totaloffset(elem).top-totaloffset(currentobj).top,2) +
+        Math.pow(totaloffset(elem).left-totaloffset(currentobj).left,2));
+      if (dist < minDist) {
+        minDist = dist;
+        minElement = elem;
+      }
+    }
+  }
+  var distaa = Math.sqrt(Math.pow(posX.get(currentobj),2)+Math.pow(posY.get(currentobj),2));
+  console.log(distaa);
+  console.log(minDist);
+  console.log(minElement);
+  if (distaa<minDist) {
+    minDist = distaa;
+    minElement = currentobj;
+  }
+  return minElement;
+}
+
 function draggablemousedown(e) {
   elem = e.target;
   lastX.set(elem,e.clientX);
@@ -29,23 +70,11 @@ function draggablemousemove() {
   elem.style.top = posY.get(currentobj) + "px";
 }
 function draggablemouseup() {
-  //find the nearest blue object and snap to it if it's close enough
-  var elems = document.getElementsByClassName("draggable blue");
-  var minDist = 65;
-  var minElement;
-  for (var i = 0;i<elems.length;i++) {
-    var elem = elems.item(i);
-    var dist = Math.sqrt(Math.pow(elem.offsetTop-currentobj.offsetTop,2) +
-      Math.pow(elem.offsetLeft-currentobj.offsetLeft,2));
-    if (dist < minDist && elem != currentobj) {
-      minDist = dist;
-      minElement = elem;
-    }
-  }
-  if (minElement) {
+  elem = getNearestElement();
+  if (elem) {
     var text = currentobj.innerHTML;
-    currentobj.innerHTML = minElement.innerHTML;
-    minElement.innerHTML = text;
+    currentobj.innerHTML = elem.innerHTML;
+    elem.innerHTML = text;
   }
   currentobj.style.left = "0px";
   currentobj.style.top = "0px";
