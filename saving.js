294a0249eade65afdd2fe5a9ada6abb69f594bb7
orangebaron <saalws@ctemc.org>
2017-09-09 19:35:49 -0400

    file saving and loading sorta

---
+++ saving.js
@@ -0,0 +1,6 @@
+function saveblocks() {
+  download("script.html", document.getElementById("draggablecontainer").innerHTML);
+}
+function loadblocks() {
+  download("script.html", document.getElementById("draggablecontainer").innerHTML);
+}
