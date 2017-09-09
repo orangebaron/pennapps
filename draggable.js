ed7ca085043bd1140f6f4a5f102fffac03f222d0
orangebaron <saalws@ctemc.org>
2017-09-09 02:08:59 -0400

    we diddit reddit
    drag and drop is just about fully functional

--- draggable.js
+++ draggable.js
@@ -3,6 +3,7 @@ var lastY = new Map();
 var posX = new Map(); //map of object to position
 var posY = new Map();
 var currentobj; //object being dragged
+var isGreen; //is currentobj green?
 
 function totaloffset(elem) { //get total offset of object
   var pos = {
@@ -17,27 +18,50 @@ function totaloffset(elem) { //get total offset of object
   return pos;
 }
 function getNearestElement() {
-  //find the nearest blue object if it's close enough
-  var elems = document.getElementsByClassName("draggable blue");
-  var minDist = 65;
-  var minElement;
-  for (var i = 0;i<elems.length;i++) {
-    var elem = elems.item(i);
-    if (elem != currentobj) {
-      var dist = Math.sqrt(Math.pow(totaloffset(elem).top-totaloffset(currentobj).top,2) +
-        Math.pow(totaloffset(elem).left-totaloffset(currentobj).left,2));
-      if (dist < minDist) {
-        minDist = dist;
-        minElement = elem;
+  if (isGreen) {
+    //find which element it should come before
+    var i = 0;
+    var allGreens = document.getElementsByClassName("draggable green");
+    while (true) {
+      if (i >= allGreens.length) { //reached the end of element list
+        if (elem == currentobj) {
+          return posY.get(currentobj) > 65 ? null : "end";
+        }
+        if (totaloffset(currentobj).top-totaloffset(elem).top > 65) {
+          return null
+        } else {
+          return "end"
+        }
+      }
+      var elem = allGreens.item(i)
+      if (elem != currentobj && totaloffset(elem).top > totaloffset(currentobj).top) {
+        return elem;
+      }
+      i++;
+    }
+  } else {
+    //find the nearest blue object if it's close enough
+    var elems = document.getElementsByClassName("draggable blue");
+    var minDist = 65;
+    var minElement;
+    for (var i = 0;i<elems.length;i++) {
+      var elem = elems.item(i);
+      if (elem != currentobj) {
+        var dist = Math.sqrt(Math.pow(totaloffset(elem).top-totaloffset(currentobj).top,2) +
+          Math.pow(totaloffset(elem).left-totaloffset(currentobj).left,2));
+        if (dist < minDist) {
+          minDist = dist;
+          minElement = elem;
+        }
       }
     }
+    var dist = Math.sqrt(Math.pow(posX.get(currentobj),2)+Math.pow(posY.get(currentobj),2));
+    if (dist<minDist) {
+      minDist = dist;
+      minElement = currentobj;
+    }
+    return minElement;
   }
-  var dist = Math.sqrt(Math.pow(posX.get(currentobj),2)+Math.pow(posY.get(currentobj),2));
-  if (dist<minDist) {
-    minDist = dist;
-    minElement = currentobj;
-  }
-  return minElement;
 }
 var highlightedDiv;
 function highlightDiv(elem,red) {
@@ -45,7 +69,19 @@ function highlightDiv(elem,red) {
     highlightedDiv.style.border = "none";
   }
   if (elem) {
-    elem.style.border = red ? "thick solid red" : "thick solid white";
+    if (red) {
+      elem.style.border = "thick solid red";
+    } else if (isGreen) {
+      if (elem == "end") {
+        var allGreens = document.getElementsByClassName("draggable green");
+        elem = allGreens.item(allGreens.length-1);
+        elem.style.borderBottom = "thick solid yellow";
+      } else {
+        elem.style.borderTop = "thick solid yellow";
+      }
+    } else {
+      elem.style.border = "thick solid yellow";
+    }
   }
   highlightedDiv = elem;
 }
@@ -53,17 +89,14 @@ function highlightDiv(elem,red) {
 function draggablemousedown(e) {
   elem = e.target;
   if (elem.innerHTML == "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;") {
-    return
+    return;
   }
+  isGreen = elem.className.search("green") != -1;
   lastX.set(elem,e.clientX);
   lastY.set(elem,e.clientY);
   currentobj = elem;
-  if (!posX.get(elem)) {
-    posX.set(elem,0);
-  }
-  if (!posY.get(elem)) {
-    posY.set(elem,0);
-  }
+  posX.set(elem,0);
+  posY.set(elem,0);
   document.onmousemove = draggablemousemove;
   document.onmouseup = draggablemouseup;
 }
@@ -78,7 +111,7 @@ function draggablemousemove() {
   currentobj.style.left = posX.get(currentobj) + "px";
   currentobj.style.top = posY.get(currentobj) + "px";
 
-  var elem = getNearestElement()
+  var elem = getNearestElement();
   if (elem) {
     highlightDiv(elem);
   } else {
@@ -87,17 +120,30 @@ function draggablemousemove() {
 }
 function draggablemouseup() {
   elem = getNearestElement();
-  if (elem) {
-    var text = currentobj.innerHTML;
-    currentobj.innerHTML = elem.innerHTML;
-    elem.innerHTML = text;
+  if (isGreen) {
+    if (elem == "end") {
+      currentobj.parentElement.appendChild(currentobj);
+      currentobj.style.left = "0px";
+      currentobj.style.top = "0px";
+    } else if (elem) {
+      console.log(elem);
+      currentobj.parentElement.insertBefore(currentobj,elem);
+      currentobj.style.left = "0px";
+      currentobj.style.top = "0px";
+    } else {
+      currentobj.parentElement.removeChild(currentobj);
+    }
   } else {
-    currentobj.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    if (elem) {
+      var text = currentobj.innerHTML;
+      currentobj.innerHTML = elem.innerHTML;
+      elem.innerHTML = text;
+    } else {
+      currentobj.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+    }
+    currentobj.style.left = "0px";
+    currentobj.style.top = "0px";
   }
-  currentobj.style.left = "0px";
-  currentobj.style.top = "0px";
-  posX.set(currentobj,0);
-  posY.set(currentobj,0);
 
   document.onmousemove = null;
   document.onmouseup = null;
