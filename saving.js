0fd04464281ddbfe583d60f2acc919dd17bac84d
orangebaron <saalws@ctemc.org>
2017-09-09 21:21:05 -0400

    loading works

--- saving.js
+++ saving.js
@@ -1,6 +1,3 @@
 function saveblocks() {
   download("script.html", document.getElementById("draggablecontainer").innerHTML);
 }
-function loadblocks() {
-  download("script.html", document.getElementById("draggablecontainer").innerHTML);
-}
